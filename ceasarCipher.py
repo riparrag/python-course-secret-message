373d4365e1c9743a8b18fc618d0fcf176f2a5d1f
riparrag <rodrigo.ipa82@gmail.com>
2024-10-26 19:07:24 -0300

    course finished

--- ceasarCipher.py
+++ ceasarCipher.py
@@ -6,7 +6,7 @@ def caesarcipherDecrypt(word, shift):
         decryptedWord += decrypt(letter, shift)
     return decryptedWord
 
-
+print('*************** Caesar course ***************')
 print('Ncevy 13 --> '+ caesarcipherDecrypt('Ncevy',13))
 print('gpvsui 25 --> '+ caesarcipherDecrypt('gpvsui',25))
 print('ugflgkg -18 --> '+ caesarcipherDecrypt('ugflgkg',-18))
